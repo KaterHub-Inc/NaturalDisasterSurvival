69d21138f47f5342746b55c1c3a101ff8543b44c
Kater <91840948+Colin4President@users.noreply.github.com>
2025-02-02 16:28:40 +0100

    Create requirements.lua

---
+++ source/requirements.lua
@@ -0,0 +1,14 @@
+--[[
+                    _       _ _    ____ _               _    
+     _____  ___ __ | | ___ (_) |_ / ___| |__   ___  ___| | __
+    / _ \ \/ / '_ \| |/ _ \| | __| |   | '_ \ / _ \/ __| |/ /
+   |  __/>  <| |_) | | (_) | | |_| |___| | | |  __/ (__|   < 
+    \___/_/\_\ .__/|_|\___/|_|\__|\____|_| |_|\___|\___|_|\_\
+             |_|                                             
+    v0.1 by KH [UNRELESSED]
+]]
+return {
+  firetouchinterest = firetouchinterest or 0;
+  fireclickdetector = fireclickdetector or 0;
+  setclipboard = setclipboard or 0;
+}
