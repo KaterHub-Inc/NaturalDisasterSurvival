1f25e7bbf9d17e90e89ce8c45c734c2c6c0908d1
Kater <91840948+Colin4President@users.noreply.github.com>
2025-02-09 16:43:27 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
-print("script ran")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "NDS Hub";
+    Text = "We are currently updating to version\n\n0.5.1";
+    Duration = 20; 
+})
+wait(3)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "NDS Hub";
+    Text = "We will be back soon!";
+    Duration = 5; 
+}) 
